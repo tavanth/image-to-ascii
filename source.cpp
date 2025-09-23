ce530af4d9f876731ee4c3c15d66f118568d9ba0
tavanth <tavanthlee@outlook.com>
2025-09-23 16:42:33 -0400

    Initialized ascii program. Checks dir for file input by user.

--- source.cpp
+++ source.cpp
@@ -1,11 +1,35 @@
 #include <iostream>
 #include <filesystem>
-using namespace std;
+#include <string>
+
 int main() {
     // First the program will check if there are any jpg or png files in the dir
-    filesystem::path directory_path = std::filesystem::current_path();
-    cout << directory_path;
-    cout << endl;
-    cout << "Hello meowmeow\n";
+   
+    std::string filename = "";
+    bool found = false;
+    std::filesystem::path directory_path = std::filesystem::current_path();
+    std::cout << "Input file name: ";
+    std::cin >> filename; 
+
+    for (const auto& entry : std::filesystem::directory_iterator(directory_path)) {
+        if (entry.is_regular_file()) {
+            std::filesystem::path file_path = entry.path();
+            if (entry.path().filename() == filename) {
+                std::string ext = file_path.extension().string();
+                std::transform(ext.begin(), ext.end(), ext.begin(), [] (unsigned char c) {return std::tolower(c); });
+                if (ext == ".jpg" || ext == ".png") {
+                    std::cout << "Found image file: " << file_path << "\n";
+                    found = true; 
+                } else {
+                    std::cout << "\nIncorrect file format (only .png or .jpg allowed).\n";
+                }
+                break; 
+            }
+        }
+    }
+    if (!found) {
+        std::cout << "Image file not found, try again.\n";
+    }
+
     return 0;
 }
